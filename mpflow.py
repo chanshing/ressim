c55df225fbe0099bc95955b762db0f77345b3bfa
Michael <cshing.m@gmail.com>
2018-11-16 23:19:47 +0000

    fix f_fn init; refactor; add BaseSolver; add checks in Grid

--- mpflow.py
+++ mpflow.py
@@ -30,12 +30,99 @@ class Grid(object):
     """
     def __init__(self, nx, ny, lx=1.0, ly=1.0):
         self.nx, self.ny = nx, ny
-        self.lx, self.ly = float(lx), float(ly)
+        self.lx, self.ly = lx, ly
 
-        self.shape = (ny, nx)
-        self.ncell = nx*ny
-        self.dx, self.dy = self.lx/nx, self.ly/ny
-        self.vol = self.dx*self.dy
+        self.update()
+
+    def update(self):
+        if all(hasattr(self, key) for key in ['nx', 'ny', 'lx', 'ly']):
+            self.shape = (self.ny, self.nx)
+            self.ncell = self.nx*self.ny
+            self.dx, self.dy = self.lx/self.nx, self.ly/self.ny
+            self.vol = self.dx*self.dy
+
+    @property
+    def nx(self):
+        return self.__nx
+
+    @property
+    def ny(self):
+        return self.__ny
+
+    @property
+    def lx(self):
+        return self.__lx
+
+    @property
+    def ly(self):
+        return self.__ly
+
+    @nx.setter
+    def nx(self, nx):
+        self.__nx = int(nx)
+        self.update()
+
+    @ny.setter
+    def ny(self, ny):
+        self.__ny = int(ny)
+        self.update()
+
+    @lx.setter
+    def lx(self, lx):
+        self.__lx = float(lx)
+        self.update()
+
+    @ly.setter
+    def ly(self, ly):
+        self.__ly = float(ly)
+        self.update()
+
+class BaseSolver(object):
+
+    @property
+    def k(self):
+        return self.__k
+
+    @property
+    def lamb(self):
+        return self.__lamb
+
+    @property
+    def q(self):
+        return self.__q
+
+    @property
+    def s(self):
+        return self.__s
+
+    @property
+    def phi(self):
+        return self.__phi
+
+    @k.setter
+    def k(self, k):
+        assert numpy.all(k > 0), "Non-positive permeability. Perhaps forgot to exp(k)?"
+        self.__k = k
+
+    @lamb.setter
+    def lamb(self, lamb):
+        assert numpy.all(lamb > 0), "Non-positive mobility"
+        self.__lamb = lamb
+
+    @q.setter
+    def q(self, q):
+        assert numpy.sum(q) == 0, "Unbalanced source term"
+        self.__q = q
+
+    @s.setter
+    def s(self, s):
+        assert numpy.all(s >= 0) and numpy.all(s <= 1), "Saturation not in [0,1]"
+        self.__s = s
+
+    @phi.setter
+    def phi(self, phi):
+        assert numpy.all(phi >= 0) and numpy.all(phi <= 1), "Porosity not in [0,1]"
+        self.__phi = phi
 
 class PressureSolver(object):
     """
@@ -57,7 +144,7 @@ class PressureSolver(object):
         means pressure values val1 at cell i1, val2 at cell i2, etc. Defaults
         to [(ny*nx/2, 0.0)], i.e. zero pressure at center of the grid.
 
-    mobi : ndarray, shape (ny, nx) | (ny*nx,) OR float, optional
+    lamb : ndarray, shape (ny, nx) | (ny*nx,) OR float, optional
         Total mobility. A single float means mobility is uniform in the
         domain. Defaults to 1.0
 
@@ -78,12 +165,12 @@ class PressureSolver(object):
         Main method that solves the pressure equation to obtain pressure and
         flux, stored at self.p and self.v
     """
-    def __init__(self, grid, k, q, mobi=1.0, diri=None):
+    def __init__(self, grid, k, q, lamb=1.0, diri=None):
         self.grid, self.k, self.q = grid, k, q
-        self.mobi = mobi
+        self.lamb = lamb
 
         if diri is None:
-            # zero at center of the grid
+            # defaults to zero at center of the grid
             n = self.grid.ncell
             self.diri = [(int(n/2), 0.0)]
 
@@ -99,23 +186,23 @@ class PressureSolver(object):
         self.__k = k
 
     @property
-    def mobi(self):
+    def lamb(self):
         return self.__mobi
 
-    @mobi.setter
-    def mobi(self, mobi):
-        assert numpy.all(mobi > 0), "Encountered non-positive mobility"
-        self.__mobi = mobi
+    @lamb.setter
+    def lamb(self, lamb):
+        assert numpy.all(lamb > 0), "Encountered non-positive mobility"
+        self.__mobi = lamb
 
     def step(self):
         grid, k = self.grid, self.k
-        mobi = self.mobi
+        lamb = self.lamb
 
         nx, ny = grid.nx, grid.ny
 
-        if isinstance(mobi, numpy.ndarray):
-            mobi = mobi.reshape(*k.shape)
-        k = k * mobi
+        if isinstance(lamb, numpy.ndarray):
+            lamb = lamb.reshape(*k.shape)
+        k = k * lamb
 
         mat, tx, ty = transmi(grid, k)
         q = copy(self.q).reshape(grid.ncell)
@@ -132,22 +219,20 @@ class PressureSolver(object):
         self.p, self.v = p, v  # update p, v
 
 class SaturationSolver(object):
-    def __init__(self, grid, q, phi, s, v=None, frac_fn=None):
-        self.grid, self.q, self.phi, self.s = grid, q, phi, s
+    def __init__(self, grid, q, phi, s, f_fn, v=None):
+        self.grid, self.q, self.phi, self.s, self.f_fn = grid, q, phi, s, f_fn
         self.v = v
-
-        # defaults to single phase flow
-        self.frac_fn = lambda s: s if frac_fn is None else frac_fn
 
     @staticmethod
     def qw(q, frac):
         # water injection, mixed production
-        return maximum(q) + frac*minimum(q)
+        qw = maximum(q,0) + frac*minimum(q,0)
+        return qw
 
     def step(self, dt):
         grid, q, phi, s = self.grid, self.q, self.phi, self.s
         v = self.v
-        frac_fn = self.frac_fn
+        f_fn = self.f_fn
 
         alpha = float(dt) / (grid.vol * phi)
         mat = convecti(grid, v)
@@ -158,7 +243,7 @@ class SaturationSolver(object):
             alpha = alpha.reshape(grid.ncenll)
 
         def residual(s1):
-            frac = frac_fn(s1)
+            frac = f_fn(s1)
             qw = self.qw(q, frac)
             return s1 - s + alpha * (mat.dot(frac) - qw)
         sol = optimize.root(residual, x0=s, method='krylov')
