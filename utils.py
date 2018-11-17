cf97a55ff0f93433a9ba322cc660209fccc0219d
Michael <cshing.m@gmail.com>
2018-11-17 00:03:37 +0000

    add BaseMobility, LinearMobility, QuadraticMobility

--- utils.py
+++ utils.py
@@ -8,20 +8,52 @@ def csr_row_set_nz_to_val(csr, row, value=0):
         raise ValueError('Matrix given must be of CSR format.')
     csr.data[csr.indptr[row]:csr.indptr[row+1]] = value
 
-class MobilityFunction(object):
-    def __init__(self, vw=1.0, vo=1.0, swc=0.0, sor=0.0, model='linear'):
-        self.vw, self.vo, self.swc, self.sor = float(vw), float(vo), float(swc), float(sor)
-        self.model = model
+class BaseMobility(object):
+    def __init__(self, vw=1.0, vo=1.0, swc=0.0, sor=0.0):
+        self.vw, self.vo, self.swc, self.sor = vw, vo, swc, sor
 
+    @property
+    def vw(self):
+        return self.__vw
+
+    @property
+    def vo(self):
+        return self.__vo
+
+    @property
+    def swc(self):
+        return self.__swc
+
+    @property
+    def sor(self):
+        return self.__sor
+
+    @vw.setter
+    def vw(self, vw):
+        self.__vw = float(vw)
+
+    @vo.setter
+    def vo(self, vo):
+        self.__vo = float(vo)
+
+    @swc.setter
+    def swc(self, swc):
+        self.__swc = float(swc)
+
+    @sor.setter
+    def sor(self, sor):
+        self.__sor = float(sor)
+
+class LinearMobility(BaseMobility):
     def __call__(self, s):
-        if self.model == 'linear':
-            _s = (s-self.swc)/(1.0-self.swc-self.sor)
-            mw = _s/self.vw
-            mo = 1.0-_s/self.vo
-        elif self.model == 'quadratic':
-            _s = (s-self.swc)/(1.0-self.swc-self.sor)
-            mw = _s**2/self.vw
-            mo = (1.0-_s)**2/self.vo
-        else: raise ValueError('choose linear or quadratic')
+        _s = (s-self.swc)/(1.0-self.swc-self.sor)
+        lamb_w = _s/self.vw
+        lamb_o = 1.0-_s/self.vo
+        return lamb_w, lamb_o
 
-        return mw, mo
+class QuadraticMobility(BaseMobility):
+    def __call__(self, s):
+        _s = (s-self.swc)/(1.0-self.swc-self.sor)
+        lamb_w = _s**2/self.vw
+        lamb_o = (1.0-_s)**2/self.vo
+        return lamb_w, lamb_o
